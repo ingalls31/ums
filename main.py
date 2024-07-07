8e61e19ac5fcb79796995aa146dad4bccc1d12c3
duchuys31 <huy52670@gmail.com>
2024-07-08 00:12:34 +0700

    feat: update log

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+import sys
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import RedirectResponse
 
+from middleware.router_logging import RouterLoggingMiddleware
 import src
 from src.config.database import engine
-from src.config.config import APP_NAME, VERSION
+from src.config.config import APP_NAME, logging_config, VERSION
 
 import src.controllers
 import src.controllers.users
 from src.models import *
 import src.models
+import logging
 
+logging.config.dictConfig(logging_config)
 
 # from src.routes import users, auth
 
@@ -32,6 +36,11 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
     allow_credentials=True
+)
+
+app.add_middleware(
+    RouterLoggingMiddleware,
+    logger=logging.getLogger(__name__)
 )
 
 app.include_router(src.controllers.users.router)
--- src/config/config.py
+++ src/config/config.py
@@ -1,4 +1,40 @@
 # TODO: Configure your app
 
+import sys
+
+
 APP_NAME = "UMS"
 VERSION = "v1.0.0"
+
+
+logging_config = {
+    "version": 1,
+    "formatters": {
+        "detailed": {
+            "format": (
+                "Time: %(asctime)s\n"
+                "Process: %(process)s\n"
+                "Level: %(levelname)s\n"
+                "Logger: %(name)s\n"
+                "Module: %(module)s\n"
+                "Function: %(funcName)s\n"
+                "Line: %(lineno)s\n"
+                "Message: %(message)s\n"
+                "------------------------------------------------------\n"
+            )
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "detailed",
+            "stream": sys.stderr,
+        }
+    },
+    "root": {
+        "level": "DEBUG",
+        "handlers": ["console"],
+        "propagate": True
+    }
+}
