049ad531c0abc3f0b88f8e8c7e0c01c2108f9289
duchuys31 <huy52670@gmail.com>
2024-08-08 23:35:32 +0700

    feat: clean

--- src/schemas/auth.py
+++ src/schemas/auth.py
@@ -16,28 +16,8 @@ class TokenData(BaseModel):
 class User(BaseModel):
     email: str = Field( unique=True, index=True)
 
-
-# class USER(User, table=True):
-#     """
-#     Represents a User in the database.
-#     """
-
-#     id: UUID | None = Field(primary_key=True, index=True, default_factory=uuid4)
-#     hashed_password: str = Field( index=True)
-#     email_verified: bool = Field(default=False)
-
-#     updated_at: datetime | None = Field(
-#         default_factory=datetime.now, sa_column_kwargs={"onupdate": datetime.now}
-#     )
-#     created_at: datetime | None = Field(default_factory=datetime.now)
-
 class RegisterUser(User):
     password: str
-
-
-class UserInDB(User):
-    id: UUID
-    hashed_password: str
 
 class UserOutput(User):
     id: UUID
